e8f2c94f98add9c5c1ad68cd32b798b80819b278
Abhishek Tripathi <abhishektripathi@Abhisheks-MacBook-Air.local>
2021-05-04 01:22:43 +0530

    Final

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import ArticleList from '../components/ArticleList';
+import { server } from '../config/index';
+
+export default function Home({ articles }) {
+  // console.log(articles);
+  return (
+    <div>
+      <h1>Welcome to Next</h1>
+      <ArticleList articles={articles} />
+    </div>
+  );
+}
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts?_limit=6`
+//   );
+
+//   const articles = await res.json();
+
+//   return {
+//     props: {
+//       articles,
+//     },
+//   };
+// };
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/articles`);
+
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
